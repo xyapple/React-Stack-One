dd86ca1b15f375913f0e77934f4c64f92dc05ab0
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-06-04 20:18:38 -0400

    Update the index.jsx and try on arr.map{}

--- app/index.jsx
+++ app/index.jsx
@@ -3,13 +3,23 @@ import {render} from 'react-dom'
 
 import './static/css/common.less'
 
+
 class Hello extends React.Component {
   render() {
+    const arr = ['a', 'b', 'c']
     return (
-      <p>Hello world! React!!</p>
-    )
+      <div>    
+       <p>Hello world! React!!</p>
+      <div>
+        {arr.map((item, index) => {
+        return <p key={index}>this is {item}</p>
+      })
+      }
+    </div>
+    </div>
+    ) }
   }
-}
+
 
 render(
   <Hello/>,
