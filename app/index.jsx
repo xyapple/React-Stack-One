062c21d9978c2a0dcc7cd249550ea1fbe6a54331
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-06-07 09:00:49 -0400

    reorginize the files and folders; Update the index.jsx(main one outside of app folder)

--- app/index.jsx
+++ app/index.jsx
@@ -3,31 +3,7 @@ import {render} from 'react-dom'
 
 import './static/css/common.less'
 
-
-class Hello extends React.Component {
-  render() {
-    const arr = ['apple', 'banana', 'coconut']
-    return (
-      <div>
-       <p>Hello React!!123456</p>
-       <br/>
-      <div>
-        <p onClick={this.clickHandler.bind(this)} style={{fontSize:'28px', display:'block'}}>Fruit Table</p>
-        <ul>
-          {arr.map((item, index) => {
-              return <li key={index}>this is {item}</li>
-          })}
-        </ul>
-    </div>
-    </div>
-    )
-  }
-    clickHandler(e){
-      e.preventDefault()
-      console.log(Date.now())
-    }
-  }
-
+import Hello from './containers/Hello/'
 
 render(
   <Hello/>,
