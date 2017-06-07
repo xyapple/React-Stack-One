e83dd7216d63cacf4e4bb9d7ecb356e508ae6678
Yin Deascentis <yd@Yins-MBP.fios-router.home>
2017-06-07 09:20:58 -0400

    Page does not show, not sure what is going on now

--- app/containers/Hello/index.jsx
+++ app/containers/Hello/index.jsx
@@ -6,28 +6,29 @@ import Recommend from './subpage/Recommend'
 import List from './subpage/List'
 
 class Hello extends React.Component {
-  constructor(props, context) {
+    constructor(props, context) {
         super(props, context);
         this.state = {
-          now: Date.now()
+            now: Date.now()
         }
     }
-    render(){
-      return (
-        <div>
-          <Header title="helloPage"/>
-          <p onClick={this.clickHandler.bind(this)}>hello world {this.state.now}</p>
-          <hr/>
-          <Carousel/>
-          <Recommend/>
-          <List/>
-        </div>
-      )
+    render() {
+        return (
+            <div>
+                <Header title="hello页面"/>
+                <p onClick={this.clickHandler.bind(this)}>hello world {this.state.now}</p>
+                <hr/>
+                <Carousel/>
+                <Recommend/>
+                <List/>
+            </div>
+        )
     }
-    clickHandler(){
-      this.setState({
-        now: Date.now()
-      })
+    clickHandler() {
+        this.setState({
+            now: Date.now()
+        })
     }
 }
+
 export default Hello
